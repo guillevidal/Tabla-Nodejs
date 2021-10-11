532f637d77eb072aa01bf2fde3cf9c52d0cee4c5
Guillermo Osvaldo Gonzalez Vidal <guillermogv@gmail.com>
2021-10-10 23:47:24 -0300

    app finalizada

---
+++ helpers/multiplicar.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+
+const crearArchivo = async (base = 5, listar = false, hasta = 10) => {
+  try {
+    let salida = "";
+    for (let i = 0; i <= hasta; i++) {
+      salida += `${base} x ${i}=${base * i}\n`;
+    }
+    if (listar) {
+      console.log("====================");
+      console.log(`Tabla del ${base}`);
+      console.log("====================");
+      console.log(salida);
+    }
+
+    //   fs.writeFile("tabla-5.txt", salida, (err) => {
+    //     if (err) throw err;
+    //     console.log("archivo creado");
+    //   });
+    // };
+    fs.writeFileSync(`./salida/tabla-${base}.txt`, salida);
+    return `tabla-${base}.txt`;
+  } catch (error) {
+    throw error;
+  }
+};
+module.exports = {
+  crearArchivo,
+};
---
+++ salida/README.md
@@ -0,0 +1,1 @@
+# Esta carpeta contiene todos los archivos .txt que creaste
